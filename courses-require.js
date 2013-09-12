bc139dd94a936f91a04bbf44592b74a0e77577f2
Ben Holmes <ben.holmes@it.ox.ac.uk>
2013-09-12 15:55:39 +0100

    Updated references to external libraries in courses-require.js

--- courses-require.js
+++ courses-require.js
@@ -2,25 +2,14 @@ window.require = {
     baseUrl: "//static.data.ox.ac.uk/courses-js-widget/",
     enforceDefine: true,
     paths: {
-      "jquery": "//static.data.ox.ac.uk/lib/jquery.min",
-      "underscore": "underscore-min",
-      "jquery.dataTables": "//static.data.ox.ac.uk/lib/datatables/js/jquery.dataTables.min",
-      "jquery-ui": "//static.data.ox.ac.uk/lib/jquery-ui/jquery-ui.min",
-      "dataox": "dataox-1.1",
-      "rdfstore": "rdf_store_min"
+      "jquery": "lib/jQuery/jquery.min",
+      "underscore": "lib/underscore/underscore-min",
+      "jquery.dataTables": "lib/dataTables/js/jquery.dataTables.min",
+      "jquery-ui": "lib/jQuery/jquery-ui.min",
     },
     shim: {
-      "underscore": {
-        exports: "_"
-      },
+      "underscore": {exports: "_"},
       "jquery.dataTables": {deps: ["jquery"]},
       "jquery-ui": {deps: ["jquery"], exports: 'require'},
-      "dataox": {
-        exports: "dataox",
-        deps: ["jquery-ui"]
-      },
-      "rdfstore": {
-          exports: "rdfstore"
-      }
     }
 };
