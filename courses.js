087d1b23b5fb26f6f19495895732a044e541e9bc
Ben Holmes <ben.holmes@it.ox.ac.uk>
2013-09-13 11:09:07 +0100

    Removed references to old libraries and now accesses the new feed, capacity for start date, title, description and provider. Doesn't dynamically generate feed parameters.

--- courses.js
+++ courses.js
@@ -14,25 +14,10 @@ THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND 
 
 */
 
-define(['jquery', 'underscore', 'rdfstore', 'dataox', 'jquery.dataTables', 'select-query', 'describe-query'], function($, _, rdfstore, DataOx, dataTables, selectQueryTemplate, describeQueryTemplate) {
-	var prefixes = {
-		dc: 'http://purl.org/dc/elements/1.1/',
-	    dcterms: 'http://purl.org/dc/terms/',
-    	mlo: 'http://purl.org/net/mlo/',
-    	org: 'http://www.w3.org/ns/org#',
-    	oxcap: 'http://purl.ox.ac.uk/oxcap/ns/',
-    	rdf: 'http://www.w3.org/1999/02/22-rdf-syntax-ns#',
-    	rdfs: 'http://www.w3.org/2000/01/rdf-schema#',
-    	skos: 'http://www.w3.org/2004/02/skos/core#',
-    	time: 'http://www.w3.org/2006/time#',
-    	xcri: 'http://xcri.org/profiles/1.2/'
-	};
-	var sparqlPrefixes = _.map(prefixes, function(i, k) { return 'PREFIX ' + k + ': <' + prefixes[k] + '>\n'; }).join('');
-	
+define(['jquery', 'underscore', 'jquery.dataTables'], function($, _, dataTables) {
+
 	var filterUndefined = function(t) { return t !== undefined; };
-	
-	var dataox = new DataOx();
-	
+
 	/* Object converter - borrowed from http://snook.ca/archives/javascript/testing_for_a_v
 	 */
 	function oc(a) {
@@ -44,8 +29,6 @@ define(['jquery', 'underscore', 'rdfstore', 'dataox', 'jquery.dataTables', 'sele
 		return o;
 	}
 
-
-
 	function paddedValue(v) {
 		if (v < 10 ) {
 			v = "0"+v;
@@ -53,22 +36,20 @@ define(['jquery', 'underscore', 'rdfstore', 'dataox', 'jquery.dataTables', 'sele
 		return v;
 	}
 
-	function now() {        
+	function now() {
 		d = new Date(); 
 		return d.getFullYear() + "-" + paddedValue(d.getMonth()+1) + "-" + paddedValue(d.getDate()) + "T" + paddedValue(d.getHours()) + ":" + paddedValue(d.getMinutes()) + ":" + paddedValue(d.getSeconds());
 	}
-	
-	
-	
+
+
 /* Our main function 
 */
 	$(function() {
 
-
 		var dataTable_css_link = $("<link>", { 
 			rel: "stylesheet", 
 			type: "text/css", 
-			href: "//static.data.ox.ac.uk/lib/dataTables/css/jquery.dataTables.css" 
+			href: "//static.data.ox.ac.uk/lib/DataTables/media/css/jquery.dataTables.css" 
 		});
 		dataTable_css_link.appendTo('head');
 
@@ -85,7 +66,7 @@ define(['jquery', 'underscore', 'rdfstore', 'dataox', 'jquery.dataTables', 'sele
 			options = {};
 			options.withoutDates = false;
 			options.title = ($(e).attr("data-title"))? $(e).attr("data-title") : "Courses";
-			options.displayColumns = ($(e).attr("data-displayColumns"))? $(e).attr("data-displayColumns") : "";
+			options.displayColumns = ($(e).attr("data-displayColumns"))? $(e).attr("data-displayColumns") : ""; // TODO trim this for whitespace first
 			
 			
 			options.units = ($(e).attr("data-providedBy") || "").split(' ');
@@ -108,76 +89,56 @@ define(['jquery', 'underscore', 'rdfstore', 'dataox', 'jquery.dataTables', 'sele
 		// constructs the query from options and sends it
 		var getData = function(e, options) {
 
-
 			var subjectFilterValue = options.skill;
 			if (options.researchMethod) {
 				subjectFilterValue = options.researchMethod;
 			}
 
-			/*
-					var unitUri  = (options.unit)? options.unit : "http://oxpoints.oucs.ox.ac.uk/id/00000000";
-					var subjectFilter = (subjectFilterValue) ? "FILTER (bound(?subject_id) && ?subject_id = <" + subjectFilterValue + ">)" : "";
-					var eligibilityFilter = (options.eligibilities)? "EXISTS { ?course oxcap:eligibility/skos:notation ?eligibility_notation. FILTER (datatype(?eligibility_notation)=oxcap:eligibility && str(?eligibility_notation) in ('" + options.eligibilities.split(" ").join("', '") + "'))}" : "";
-					var startingBeforeFilter = (options.startingBefore && !options.noDates) ? "FILTER (bound(?presentation_start_dateTime) && ((datatype(?presentation_start_dateTime) = xsd:date && ?presentation_start_dateTime <= \"" + options.startingBefore.substring(0, 10) + "\"^^xsd:date) || (tz(?presentation_start_dateTime) && ?presentation_start_dateTime <= \"" + options.startingBefore + "\"^^xsd:dateTime) || (?presentation_start_dateTime <= \"" + options.startingBefore.substring(0, 19) + "\")))" : "";
-					var startingAfterFilter = (options.startingAfter && !options.noDates) ? "FILTER (bound(?presentation_start_dateTime) && ((datatype(?presentation_start_dateTime) = xsd:date && ?presentation_start_dateTime >= \"" + options.startingAfter.substring(0, 10) + "\"^^xsd:date) || (tz(?presentation_start_dateTime) && ?presentation_start_dateTime >= \"" + options.startingAfter + "\"^^xsd:dateTime) || (?presentation_start_dateTime >= \"" + options.startingAfter.substring(0, 19) + "\")))" : "";
-					var noDatesFilter = (options.noDates) ? "FILTER (!bound(?presentation_start_dateTime))" : "";
-					var applyUnitlInFuture = "FILTER !!!!TODO!!!!";
-			 */
+			// TODO handle some options please :)
 
-			dataox.sparql({
-				query: sparqlPrefixes + selectQueryTemplate(options),
-				type: "resultset",
-				ajaxOptions: {
-					beforeSend: function() { $(e).children(".courses-widget-wait").show(); }
-				}
-			}, function(results) {
-				var presentations = _.map(results.results.bindings, function(r) { return r.presentation.value; });
-				dataox.sparql({
-					query: sparqlPrefixes + describeQueryTemplate({presentations: presentations}),
-					type: "graph",
-					ajaxOptions: {
-						complete: function() { $(e).children(".courses-widget-wait").hide(); }
+			$.ajax({
+					url : '//data.ox.ac.uk/search/?format=js&q=%2A+NOT+offeredBy.label%3A%22Department+of+Continuing+Education%22&type=presentation',
+					jsonpCallback : 'callback',
+					dataType: 'jsonp',
+					beforeSend : function() {
+						$(e).children(".courses-widget-wait").show();
+					},
+					success : function(json) {
+						handleData(e, options, json)
+					},
+					error : function(e) {
+						console.log(e.message)
 					}
-				}, function(store) {
-					handleData(e, options, presentations, store);
-				});
 			});
 
 		};
 
 		// handles the query results 
-		var handleData = function(e, options, presentations, store) {
-			for (var k in prefixes)
-				store.setPrefix(k, prefixes[k]);
+		var handleData = function(e, options, results) {
 
+			// TODO Are these results safe to output as html?
 
-			/* *** uncomment for debugging: *** 
-                if (window.jsDump) {
-		    	  jsDump.HTML = true;
-		    	  var dumped = jsDump.parse(data);
-		    	  $('#debug').html(dumped);
-                }
-			    /* ******************************** */
-
-			var columnsAvailable = { 'start': '<th class="course-presentation-start">Start date</th>',
+			var columnsAvailable = { 
+					'start': '<th class="course-presentation-start">Start date</th>',
 					'title': '<th class="course-title">Title</th>',
 					'subject': '<th class="course-subject">Subject(s)</th>',
+					'venue': '<th class="course-presentation-venue">Venue</th>',
 					'provider': '<th class="course-provider">Provider</th>',
 					'description': '<th class="course-description">Description</th>',
-					'venue': '<th class="course-presentation-venue">Venue</th>',
 					'eligibility': '<th class="course-eligibility">Eligibility</th>',
 					'info': '<th class="course-info">Further information</th>',
 			};
 
 			var tableHeaderCells = "";
 			var columnsToDisplay = {};
+
 			if (options.displayColumns !== "") {
-				columnsToDisplay = options.displayColumns.split(" ");
-				columnsToDisplay = oc(columnsToDisplay);
+				columnsToDisplay = oc(options.displayColumns.split(" "));
 			} else {
 				// if no columns specified, default to all available
 				columnsToDisplay = columnsAvailable;
 			}
+
 			for (var i in columnsToDisplay) {
 				tableHeaderCells += columnsAvailable[i];
 			}
@@ -191,121 +152,98 @@ define(['jquery', 'underscore', 'rdfstore', 'dataox', 'jquery.dataTables', 'sele
 			var months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
 			var weekday = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
 
-			var r = _.bind(store.rdf.resolve, store.rdf);
+			presentations = results.hits.hits;
 
-			//var bindings = dataox.objectify(data.results.bindings, ["subject", "presentation"]);
-			store.graph(function(success, graph) {
-				var all = function(s, p, o) {
-					return graph.match(s, p, o).toArray();
-				};
-				var get = function(s, p, o) {
-					var matches = all(s, p, o);
-					return matches.length ? matches[0] : null;
-				};
-				var oneOf = function(s) {
-					for (var i=1; i<arguments.length; i++) {
-						var o = get(s, r(arguments[i]));
-						if (o)
-							return o.object;
+			for (var i=0, max=presentations.length; i<max; i++) {
+
+				var presentation = presentations[i]._source;
+				var course = presentation.course.uri;
+
+				var cells = {};
+
+				var start = new Date(presentation.start.time * 1000);
+				if (start && columnsToDisplay.start) {
+					var startTime = ""; 
+					var startHour = start.getHours();
+					if (startHour > 2) {
+						var startMinutes = start.getMinutes();
+						startTime = " " + (startHour > 12 ? startHour - 12 : startHour) + ":" + (startMinutes < 10 ? "0" : "") + startMinutes + " " + (startHour > 11 ? "PM" : "AM");
 					}
-					return null;
-				};
-				
-
-				for (var i=0, max=presentations.length; i<max; i++) {
-					var presentation = presentations[i];
-					var course = get(null, r('mlo:specifies'), presentation).subject;
-
-					// for each result:
-					//var  = data.results.bindings[i];
-					var cells = {};
-
-					var start = get(presentation, r('mlo:start'), null);
-					if (start && columnsToDisplay.start) {
-						start = start.object;
-						start = oneOf(start, 'time:inXSDDateTime', 'rdf:value').valueOf();
-						var startTime = ""; 
-						var startHour = start.getHours();
-						if (startHour > 2) {
-						  var startMinutes = start.getMinutes();
-							startTime = " " + (startHour > 12 ? startHour - 12 : startHour) + ":" + (startMinutes < 10 ? "0" : "") + startMinutes + " " + (startHour > 11 ? "PM" : "AM");
-						}
-						var startFormatted = weekday[start.getDay()] + " " + start.getDate()+ " " + months[start.getMonth()]  + " " + start.getFullYear() + startTime;
-						cells.start = startFormatted;
-					}
-
-					var title = oneOf(course, 'dcterms:title', 'rdfs:label');
-					var applyTo = get(presentation, r('xcri:applyTo'));
-					if ('title' in columnsToDisplay) {
-						title = title ? title.valueOf() : '—';
-						if (applyTo)
-							cells.title = $('<a>', {title: presentation, href: applyTo.object.valueOf()}).text(title);
-						else
-							cells.title = $('<span>', {title: presentation}).text(title);
-					}
-
-					if ('subject' in columnsToDisplay) {
-						var subjects = _.filter(_.map(all(course, r('dcterms:subject')), function(t) {
-							var subject = t.object;
-							// Ignore JACS codes
-							if (subject.nominalValue.indexOf('http://jacs.dataincubator.org/') !== 0)
-								return undefined;
-							return get(subject, r('skos:prefLabel')).object.valueOf();
-						}), filterUndefined);
-						cells.subject = $('<span>').text(subjects.join(', '));
-					}
-
-					var venue = oneOf(presentation, 'xcri:venue');
-					if (venue && 'venue' in columnsToDisplay) {
-						var venueLabel = oneOf(venue, 'rdfs:label', 'skos:prefLabel', 'dc:title');
-						cells.venue = $('<span>').text(venueLabel ? venueLabel.valueOf() : "—");
-					}
-
-					var provider = get(null, 'mlo:offers', course);
-					if (provider && 'provider' in columnsToDisplay) {
-						var providerLabel = oneOf(provider, 'rdfs:label', 'skos:prefLabel', 'dc:title');
-						cells.provider = $('<span>').text(providerLabel ? providerLabel.valueOf() : "—");
-					}
-
-					var row = $("<tr>");
-
-					for(var column in columnsToDisplay) {
-						row.append($('<td>').append(cells[column]));
-					}
-					tbody.append(row);
-					continue;
-
-
-					if ('description' in columnsToDisplay) {
-						var courseDescription = "";
-						if (binding.courseDescription) {
-							courseDescription = binding.courseDescription.value;
-						}
-						rowToDisplay.description = '<td class="course-description">' + courseDescription + '</td>';
-					}
-
-					if ('eligibility' in columnsToDisplay) {
-						var courseEligibility = "";
-						if (binding.presentationRegulations) {
-							courseEligibility = binding.presentationRegulations.value;
-						}
-						rowToDisplay.eligibility = '<td class="course-eligibility">' + courseEligibility + '</td>';
-					}
-					if ('info' in columnsToDisplay) {
-						var courseInfo = "";
-						if (binding.courseURL) {
-							courseInfo = binding.courseURL.value;
-						}
-						rowToDisplay.info = '<td class="course-info">' + courseInfo + '</td>';
-					}
-
-
+					var startFormatted = weekday[start.getDay()] + " " + start.getDate()+ " " + months[start.getMonth()]  + " " + start.getFullYear() + startTime;
+					cells.start = startFormatted;
 				}
-			
-			});
+
+				var title = presentation.label
+				var applyTo = presentation.applyTo;     // TODO we are not getting applyTo yet in the field
+
+				if ('title' in columnsToDisplay) {
+					title = title ? title.valueOf() : '—';
+					if (applyTo)
+						cells.title = $('<a>', {title: title, href: applyTo}).text(title);
+					else
+						cells.title = $('<span>', {title: title}).text(title);
+				}
+
+				/* TODO we are not getting subjects yet
+				if ('subject' in columnsToDisplay) {
+					var subjects = _.filter(_.map(all(course, r('dcterms:subject')), function(t) {
+						var subject = t.object;
+						// Ignore JACS codes
+						if (subject.nominalValue.indexOf('http://jacs.dataincubator.org/') !== 0)
+							return undefined;
+						return get(subject, r('skos:prefLabel')).object.valueOf();
+					}), filterUndefined);
+					cells.subject = $('<span>').text(subjects.join(', '));
+				}
+				*/
+
+				/* TODO we are not getting venues yet
+				var venue = oneOf(presentation, 'xcri:venue');
+				if (venue && 'venue' in columnsToDisplay) {
+					var venueLabel = oneOf(venue, 'rdfs:label', 'skos:prefLabel', 'dc:title');
+					cells.venue = $('<span>').text(venueLabel ? venueLabel.valueOf() : "—");
+				}
+				*/
+
+				if ('provider' in columnsToDisplay) {
+					var provider = presentation.offeredBy.label
+					cells.provider = provider ? provider : '-';
+				}
+
+				if ('description' in columnsToDisplay) {
+					var description = presentation.description
+					cells.description = '<td class="course-description">' + description + '</td>';
+				}
+
+
+				var row = $("<tr>");
+
+				for(var column in columnsToDisplay) {
+					row.append($('<td>').append(cells[column]));
+				}
+				tbody.append(row);
+				continue; // TODO what is this doing here? get rid of it.
+
+
+				if ('eligibility' in columnsToDisplay) {
+					var courseEligibility = "";
+					if (binding.presentationRegulations) {
+						courseEligibility = binding.presentationRegulations.value;
+					}
+					rowToDisplay.eligibility = '<td class="course-eligibility">' + courseEligibility + '</td>';
+				}
+				if ('info' in columnsToDisplay) {
+					var courseInfo = "";
+					if (binding.courseURL) {
+						courseInfo = binding.courseURL.value;
+					}
+					rowToDisplay.info = '<td class="course-info">' + courseInfo + '</td>';
+				}
+
+
+			}
 
 			var tableFoot = '</tbody></table>';
-
 
 			var linkTitle = (options.withoutDates)? "courses with specific dates" : "courses without specific dates";
 			var $noDatesToggle = $('<a class="courses-widget-no-date-toggle-link" href="#">' + linkTitle + '</a>').click(function () {
@@ -313,7 +251,7 @@ define(['jquery', 'underscore', 'rdfstore', 'dataox', 'jquery.dataTables', 'sele
 				$(e).children('.course-results-table').remove();
 				$(e).children('.dataTables_wrapper').remove();
 				$(this).remove(); 
-				getData(e, options);	           
+				getData(e, options);
 				return false;
 			});
 
@@ -334,8 +272,11 @@ define(['jquery', 'underscore', 'rdfstore', 'dataox', 'jquery.dataTables', 'sele
 			}
 			$(e).children(".course-results-table").dataTable( {
 				aoColumnDefs: dataTablesColumnsConfig,
-				iDisplayLength: 25
+				"bPaginate": false
 			} );
+
+			$(e).children(".courses-widget-wait").hide();
+
 
 		};
 
